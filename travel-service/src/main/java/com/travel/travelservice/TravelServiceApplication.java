9aec3e4b1dccac2bc0b05e2c1ad2b0850ddbe33b
jianping5 <2712748478@qq.com>
2023-03-16 21:02:42 +0800

    Initial commit

---
+++ travel-service/src/main/java/com/travel/travelservice/TravelServiceApplication.java
@@ -0,0 +1,21 @@
+package com.travel.travelservice;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+// todo 如需开启 Redis，须移除 exclude 中的内容
+@SpringBootApplication(exclude = {RedisAutoConfiguration.class})
+@MapperScan("com.travel.travel.mapper")
+@EnableScheduling
+@EnableAspectJAutoProxy(proxyTargetClass = true, exposeProxy = true)
+public class TravelServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TravelServiceApplication.class, args);
+    }
+
+}
